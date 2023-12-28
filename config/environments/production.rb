a1703e528ab20753ec6a62517515ca9f3dbb6238
Joel AZEMAR <joel.azemar@gmail.com>
2023-12-28 17:41:54 +0100

    Rename to Notes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "rails_app_test_6_production"
+  # config.active_job.queue_name_prefix = "notes_production"
 
   config.action_mailer.perform_caching = false
 
