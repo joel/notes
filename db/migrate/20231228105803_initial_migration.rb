b003f46a5077951fa986e72b158a50da6104e723
Joel AZEMAR <joel.azemar@gmail.com>
2023-12-28 11:58:03 +0100

    Create database and initial migration

---
+++ db/migrate/20231228105803_initial_migration.rb
@@ -0,0 +1,5 @@
+# Source: https://github.com/rails/rails/blob/v7.1.2/activerecord/lib/rails/generators/active_record/migration/templates/migration.rb.tt
+class InitialMigration < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
